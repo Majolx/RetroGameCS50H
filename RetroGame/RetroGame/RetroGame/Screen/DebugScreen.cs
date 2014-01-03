cdbc7e64141d3f04d5419825ec01ca69c73ccf15
Majolx <majolx@github.com>
2014-01-02 17:14:12 -0800

    First commit

---
+++ RetroGame/RetroGame/RetroGame/Screen/DebugScreen.cs
@@ -0,0 +1,59 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using RetroGame.Screen;
+#endregion
+
+namespace RetroGame.Screen
+{
+    class DebugScreen : MenuScreen
+    {
+        #region Initialization
+
+
+        /// <summary>
+        /// Constructor fills in the menu contents.
+        /// </summary>
+        public DebugScreen()
+            : base("Debug!1")
+        {
+            // Create our menu entries.
+            MenuEntry asteroidMenuEntry = new MenuEntry("Asteroid");
+            MenuEntry backMenuEntry = new MenuEntry("Go Back");
+
+            // Hook up menu event handlers.
+            asteroidMenuEntry.Selected += AsteroidMenuEntrySelected;
+            backMenuEntry.Selected += OnCancel;
+
+            // Add entries to the menu.
+            MenuEntries.Add(asteroidMenuEntry);
+            MenuEntries.Add(backMenuEntry);
+        }
+
+
+        #endregion
+
+        #region Handle Input
+
+        void AsteroidMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new AsteroidGameScreen(), e.PlayerIndex);
+        }
+
+
+        /// <summary>
+        /// When the user cancels the main menu, ask if they want to exit the sample.
+        /// </summary>
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            this.ExitScreen();
+        }
+
+
+        #endregion
+    }
+}
---
+++ RetroGame/RetroGame/RetroGame/Screen/Games/AsteroidGameScreen.cs
@@ -0,0 +1,108 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace RetroGame.Screen
+{
+    class AsteroidGameScreen : GameScreen
+    {
+        #region Fields
+
+        /// <summary>
+        /// A screen-specific content manager.
+        /// </summary>
+        public ContentManager content;
+
+        #endregion
+
+        #region Initialization
+        
+        /// <summary>
+        /// The constructor for the Asteroid game screen
+        /// </summary>
+        public AsteroidGameScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+
+        /// <summary>
+        /// Loads game content.  Contains its own content manager to keep
+        /// the game lightweight.
+        /// </summary>
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+            base.LoadContent();
+        }
+
+
+        /// <summary>
+        /// Unloads game content.  Make sure to close up any files and
+        /// free up any resources before exiting.
+        /// </summary>
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+
+        #endregion
+
+        #region Handle Input
+
+
+        /// <summary>
+        /// Handles the input for this screen.
+        /// </summary>
+        public override void HandleInput(InputState input)
+        {
+            base.HandleInput(input);
+        }
+
+
+        #endregion
+
+        #region Update and Draw
+
+
+        /// <summary>
+        /// The screen's update loop.  Place any update logic here.
+        /// </summary>
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+
+        /// <summary>
+        /// The screen's draw loop.  Draw all artifacts here.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(ScreenManager.Font, "There will be asteroids here soon...", new Vector2 (0, 0), Color.White);
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+
+
+        #endregion
+    }
+}
